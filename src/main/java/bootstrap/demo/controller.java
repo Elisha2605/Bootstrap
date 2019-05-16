9b75efe05968bbb054faa2257b3728fb60629cc8
Elisha <alvinjohnson@Elishas-MacBook-Pro.local>
2019-05-16 10:53:17 +0200

    2nd commit

---
+++ src/main/java/bootstrap/demo/controller.java
@@ -0,0 +1,14 @@
+package bootstrap.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
